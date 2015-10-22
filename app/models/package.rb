9aaff09a0991132c9afbb4e1627c58058cbe5bb1
Anton Volkov <choixer@gmail.com>
2015-10-22 16:56:56 +0200

    Remove unused method from package

--- app/models/package.rb
+++ app/models/package.rb
@@ -3,14 +3,6 @@ class Package < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
 
-  def self.create_from_package_info!(info)
-    Package.create!(
-      name:             info["Package"],
-      current_version:  info["Version"],
-      payload:          info
-    )
-  end
-
   def version
     versions.find_by_version(current_version)
   end
--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -1,23 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Package, type: :model do
-  describe '#create_from_package_info!' do
-    let(:package_info) do
-      {
-        "Package"=> "A3",
-        "Version"=> "1.0.0",
-        "Depends"=> "R (>= 2.15.0), xtable, pbapply",
-        "Suggests"=>  "randomForest, e1071", "License"=>"GPL (>= 2)", "NeedsCompilation"=>"no"
-      }
-    end
-
-    it 'creates Package' do
-      expect(Package).to receive(:create!).with(
-        name: "A3",
-        current_version: "1.0.0",
-        payload: package_info
-      )
-      Package.create_from_package_info!(package_info)
-    end
-  end
 end
