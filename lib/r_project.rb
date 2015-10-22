51b3e1f70ea9d049f3ee1da80ef1d18c0c517754
Anton Volkov <choixer@gmail.com>
2015-10-22 11:12:12 +0200

    Fetch list of packages from the server

--- lib/r_project.rb
+++ lib/r_project.rb
@@ -1,5 +1,17 @@
-module RProject
-  def self.packages
-    []
+require 'open-uri'
+
+class RProject
+  def intitialize
+  end
+
+  def packages
+    fetch_package_list
+    # []
+  end
+
+  private
+
+  def fetch_package_list
+    @packages ||= open('http://cran.r-project.org/src/contrib/PACKAGES').read
   end
 end
--- spec/lib/r_project_spec.rb
+++ spec/lib/r_project_spec.rb
@@ -1,11 +1,38 @@
 require 'rails_helper'
 
 describe RProject do
-  describe '#packages' do
-    subject { RProject.packages }
+  let(:packages) do
+    <<-PACKAGE
+    Package: A3
+    Version: 1.0.0
+    Depends: R (>= 2.15.0), xtable, pbapply
+    Suggests: randomForest, e1071
+    License: GPL (>= 2)
+    NeedsCompilation: no
+
+    Package: abbyyR
+    Version: 0.2
+    Depends: R (>= 3.2.0)
+    Imports: httr, XML, curl, RecordLinkage, readr
+    License: GPL (>= 2)
+    NeedsCompilation: no
+
+    Package: abc
+    Version: 2.1
+    Depends: R (>= 2.10), abc.data, nnet, quantreg, MASS, locfit
+    License: GPL (>= 3)
+    NeedsCompilation: no
+    PACKAGE
+  end
+
+  describe '.packages' do
+    subject(:r_project) { RProject.new }
+    before do
+      allow(r_project).to receive(:fetch_package_list).and_return(packages)
+    end
 
     it 'returns list of current packages' do
-      expect(subject).to eq([])
+      expect(r_project.packages).to eq(packages)
     end
   end
 end
